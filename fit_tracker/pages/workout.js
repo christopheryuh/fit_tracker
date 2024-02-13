9cd61873a95997b6cc2be41fd48d07cd6fc1403d
Chris Yuh <christopheryuh@MacBook-Air.localdomain>
2024-02-13 08:32:54 -0500

    workout progress

--- fit_tracker/pages/workout.js
+++ fit_tracker/pages/workout.js
@@ -1,59 +1,49 @@
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
+import { useState } from 'react';
 import Link from 'next/link'
 
 const inter = Inter({ subsets: ["latin"] });
 
-let data = []
+const test_list = [];
 
-class Set {
-    constructor(){
-        this.reps = ""
-        this.weight = ""
-    }
+function WorkoutRows() {
+  // State to hold the list of components
+  const [rows, setRow] = useState([]);
+
+  // Function to add a new component to the list
+  const addRow = () => {
+    // Here, you can add whatever component you want to add to the list
+    const newRow = <p key={rows.length}>New Component</p>;
+    test_list.push(rows.lengths)
+    setRow([...rows, newRow]);
+  };
+
+  return (
+    <div>
+      <h2>Component List</h2>
+      {/* Render the list of components */}
+      {rows.map(row => row)}
+      {/* Button to add a new component */}
+      <button onClick={addRow}>Add Row</button>
+      <p>{test_list}</p>
+    </div>
+  );
 }
 
-
-function ExerciseSection({name}){
-    return (
-        <h2>{name}</h2>
-
-    )
-}
-
-function RenderData({data}){
-    return ({data})
-}
-
-
-// function SetRow({reps,weight}){
-//     return ()
-// }
-
-function AddExercise(){
-    return (
-        <p>Add New Exercise</p><input></input>
-    );
-}
 
 
 export default function Home() {
-
-    let exercise_names = [];
-    let sets = [];
-    let reps = [];
   return (
     <div>
     <h1>Workout</h1>
     <Link href="/">home</Link>
     <hr></hr>
     <div id="list">
-
-    <Exercise name="bench"></Exercise>
+    
+    <WorkoutRows></WorkoutRows>
 
     </div>
-    <RenderData data={data}></RenderData>
     </div>
   );
 }
